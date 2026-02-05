5b65a544fb63d0a8ce11b0035d65081a2b71885e
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2026-02-05 10:48:57 +0000

    created a submission

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ const url1=images[0].url;
 const url2=images[1].url;
 const url3=images[2].url;
 
-outputScreen.innerHTML=`<div id='loading'>Loading...</div>`
 
+btn.addEventListener('click',()=>{
+outputScreen.innerHTML=`<div id='loading'>Loading...</div>`;
 Promise.all([fetch(url1),fetch(url2),fetch(url3)])
 .then((data)=>{
     outputScreen.innerHTML='';
@@ -32,3 +33,5 @@ Promise.all([fetch(url1),fetch(url2),fetch(url3)])
     outputScreen.innerHTML=`<div id='error'>${err.name}:${err.message}<div>`;
 
 })
+})
+
