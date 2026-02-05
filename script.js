94891f6d3941c0ba8742d64d22dd66f52dee7edd
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2026-02-05 10:40:40 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
+const outputScreen = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
 const images = [
@@ -7,3 +7,28 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+const url1=images[0].url;
+const url2=images[1].url;
+const url3=images[2].url;
+
+outputScreen.innerHTML=`<div id='loading'>Loading...</div>`
+
+Promise.all([fetch(url1),fetch(url2),fetch(url3)])
+.then((data)=>{
+    outputScreen.innerHTML='';
+
+    data.forEach((item)=>{
+        if(!item.ok){
+            throw new Error('Network issue')
+        }
+
+        let val=item.url;
+        outputScreen.innerHTML+=`<img src=${val}>`
+    })
+})
+.catch(err=>{
+    outputScreen.innerHTML=`<div id='error'>${err.name}:${err.message}<div>`;
+
+})
